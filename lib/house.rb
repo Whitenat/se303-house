8311bedf91f42148bbaa5b1b6637a3944f8d5442
Natashia White <whitenat@oregonstate.edu>
2021-04-15 10:28:54 -0700

    add case statment to return new verse if passed value 2

--- lib/house.rb
+++ lib/house.rb
@@ -1,7 +1,12 @@
 class House
 
-	def line(_)
-		return "This is the house that Jack built.\n"
+	def line(verse)
+		case verse
+		when 1
+			return "This is the house that Jack built.\n"
+		when 2
+			return "This is the malt that lay in the house that Jack built.\n"
+		end
 	end
 
 end
