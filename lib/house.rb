d5b9b34986ffd82841b328ca8586ae6a585bcfd2
Natashia White <whitenat@oregonstate.edu>
2021-04-15 10:31:32 -0700

    add case for verse 4

--- lib/house.rb
+++ lib/house.rb
@@ -8,6 +8,8 @@ class House
 			return "This is the malt that lay in the house that Jack built.\n"
 		when 3
 			return "This is the rat that ate the malt that lay in the house that Jack built.\n"
+		when 4
+			return "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
