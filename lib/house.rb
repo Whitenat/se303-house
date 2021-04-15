edddf3fd42b2cdce2c2f337c0f0e15d13f1cd3ac
Natashia White <whitenat@oregonstate.edu>
2021-04-15 10:33:30 -0700

    add case for verse 6

--- lib/house.rb
+++ lib/house.rb
@@ -14,6 +14,8 @@ class House
 			return "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 6
 			return "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+		when 7 
+			return "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
