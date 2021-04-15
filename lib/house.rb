7d9886d4566fefd7f63c8f3933cd85761bde06a4
Natashia White <whitenat@oregonstate.edu>
2021-04-15 10:32:55 -0700

    add case for verse 6

--- lib/house.rb
+++ lib/house.rb
@@ -12,6 +12,8 @@ class House
 			return "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 5
 			return "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+		when 6
+			return "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
