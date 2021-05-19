ab9a097c6fa84cbcf47d603bd5862d3abaacd1cf
Natashia White <whitenat@oregonstate.edu>
2021-05-18 17:40:53 -0700

    back to green

--- lib/house.rb
+++ lib/house.rb
@@ -23,7 +23,7 @@ class House
 		when 10
 			return "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 11
-			return "This is #{(phrase(verse))}\n"
+			return "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 12
 			return "This is the horse and the hound and the horn that belonged to #{(phrase(verse))}\n"
 		end
