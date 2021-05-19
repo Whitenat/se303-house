8ad1ab80ccd16276a276ca76af02dfefe5c08121
Natashia White <whitenat@oregonstate.edu>
2021-05-18 18:29:12 -0700

    make case 1 match the else case

--- lib/house.rb
+++ lib/house.rb
@@ -3,13 +3,9 @@ class House
 	def line(verse)
 		case verse
 		when 1
-			return "This is the house that Jack built.\n"
-		when 2
-			return "This is #{phrase(verse)}#{phrase(verse-1)}#{phrase(verse-2)}#{phrase(verse-3)}#{phrase(verse-4)}#{phrase(verse-5)}#{phrase(verse-6)}#{phrase(verse-7)}#{phrase(verse-8)}#{phrase(verse-9)}the malt that lay in the house that Jack built.\n"
-		when 3
-			return "This is #{phrase(verse)}#{phrase(verse-1)}#{phrase(verse-2)}#{phrase(verse-3)}#{phrase(verse-4)}#{phrase(verse-5)}#{phrase(verse-6)}#{phrase(verse-7)}#{phrase(verse-8)}#{phrase(verse-9)}the malt that lay in the house that Jack built.\n"
+			return "This is #{phrase(verse)}#{phrase(verse-1)}#{phrase(verse-2)}#{phrase(verse-3)}#{phrase(verse-4)}#{phrase(verse-5)}#{phrase(verse-6)}#{phrase(verse-7)}#{phrase(verse-8)}#{phrase(verse-9)}#{phrase(verse-10)}the house that Jack built.\n"
 		else
-			return "This is #{phrase(verse)}#{phrase(verse-1)}#{phrase(verse-2)}#{phrase(verse-3)}#{phrase(verse-4)}#{phrase(verse-5)}#{phrase(verse-6)}#{phrase(verse-7)}#{phrase(verse-8)}#{phrase(verse-9)}the malt that lay in the house that Jack built.\n"
+			return "This is #{phrase(verse)}#{phrase(verse-1)}#{phrase(verse-2)}#{phrase(verse-3)}#{phrase(verse-4)}#{phrase(verse-5)}#{phrase(verse-6)}#{phrase(verse-7)}#{phrase(verse-8)}#{phrase(verse-9)}#{phrase(verse-10)}the house that Jack built.\n"
 		end
 	end
 
@@ -39,6 +35,8 @@ class House
 			"the cat that killed "
 		when 3
 			"the rat that ate "
+		when 2
+			"the malt that lay in "
 		else
 			""
 		end
