7fab798f558113822adf340ea8248c56c7a97758
Natashia White <whitenat@oregonstate.edu>
2021-05-18 17:42:23 -0700

    add case statement to phrase with next most similar line

--- lib/house.rb
+++ lib/house.rb
@@ -34,7 +34,12 @@ class House
 	end
 
 	def phrase(number)
-		"the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+		case number
+		when 12
+			"the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+		when 11
+			"the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built."
+		end
 	end
 
 end
