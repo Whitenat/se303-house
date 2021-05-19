a2b9f337835454d4d5c0d9a7d78d5248b02f4133
Natashia White <whitenat@oregonstate.edu>
2021-05-18 17:58:59 -0700

    extract new difference into phrase and make 11 and 12 the same

--- lib/house.rb
+++ lib/house.rb
@@ -23,9 +23,9 @@ class House
 		when 10
 			return "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 11
-			return "This is #{(phrase(verse))}the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			return "This is #{(phrase(verse))}#{(phrase(verse-1))}the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		when 12
-			return "This is #{(phrase(verse))}the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+			return "This is #{(phrase(verse))}#{(phrase(verse-1))}the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
 		end
 	end
 
@@ -37,6 +37,8 @@ class House
 		case number
 		when 12
 			"the horse and the hound and the horn that belonged to "
+		when 11
+			"the farmer sowing his corn that kept "
 		else
 			""
 		end
