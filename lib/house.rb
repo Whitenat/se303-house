c4a6311a4a4e68e6b6d3dcab0ea7ca9797105f6d
Natashia White <whitenat@oregonstate.edu>
2021-05-27 19:23:25 -0700

    elevate pieces to be a member var

--- lib/house.rb
+++ lib/house.rb
@@ -1,4 +1,17 @@
 class House
+	@@pieces = [	
+		"the malt that lay in ",
+		"the rat that ate ",
+		"the cat that killed ",
+		"the dog that worried ",
+		"the cow with the crumpled horn that tossed ",
+		"the maiden all forlorn that milked ",
+		"the man all tattered and torn that kissed ",
+		"the priest all shaven and shorn that married ",
+		"the rooster that crowed in the morn that woke ",
+		"the farmer sowing his corn that kept ",
+		"the horse and the hound and the horn that belonged to "
+	]
 
 	def line(verse)
 		"#{intro()}#{verse.downto(1).collect { |i| phrase(i) }.join("")}the house that Jack built.\n"
