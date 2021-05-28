7e7682750d46e6997a7fcbcb7b5fb2e8dd55431c
Natashia White <whitenat@oregonstate.edu>
2021-05-27 19:19:53 -0700

    remove blank like from pieces array

--- lib/house.rb
+++ lib/house.rb
@@ -1,20 +1,4 @@
 class House
-	@@pieces = [	
-		"the malt that lay in ",
-		"the rat that ate ",
-		"the cat that killed ",
-		"the dog that worried ",
-		"the cow with the crumpled horn that tossed ",
-		"the maiden all forlorn that milked ",
-		"the man all tattered and torn that kissed ",
-		"the priest all shaven and shorn that married ",
-		"the rooster that crowed in the morn that woke ",
-		"the farmer sowing his corn that kept ",
-		"the horse and the hound and the horn that belonged to "
-	]
-	def initialize
-		@@pieces.unshift("")
-	end
 
 	def line(verse)
 		"#{intro()}#{verse.downto(1).collect { |i| phrase(i) }.join("")}the house that Jack built.\n"
@@ -29,9 +13,8 @@ class House
 	end
 
 	def phrase(number)
-		return ""if number < 0
+		return ""if number < 2
 		pieces = [	
-		"",	
 		"the malt that lay in ",
 		"the rat that ate ",
 		"the cat that killed ",
@@ -44,7 +27,7 @@ class House
 		"the farmer sowing his corn that kept ",
 		"the horse and the hound and the horn that belonged to "
 		]
-		pieces[number -1]
+		pieces[number -2]
 	end
 
 end
