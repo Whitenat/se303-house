adce60445f88eaa77575fe995ac63254a0cfd480
Natashia White <whitenat@oregonstate.edu>
2021-05-28 18:42:25 -0700

    update pieces method to start using noun method

--- lib/house.rb
+++ lib/house.rb
@@ -31,17 +31,17 @@ class House
 	end
 	def pieces 
 		[	
-			"the malt that lay in ",
-			"the rat that ate ",
-			"the cat that killed ",
-			"the dog that worried ",
-			"the cow with the crumpled horn that tossed ",
-			"the maiden all forlorn that milked ",
-			"the man all tattered and torn that kissed ",
-			"the priest all shaven and shorn that married ",
-			"the rooster that crowed in the morn that woke ",
-			"the farmer sowing his corn that kept ",
-			"the horse and the hound and the horn that belonged to "
+			"#{noun[0]}that lay in ",
+			"#{noun[1]}that ate ",
+			"#{noun[2]}that killed ",
+			"#{noun[3]}that worried ",
+			"#{noun[4]}that tossed ",
+			"#{noun[5]}that milked ",
+			"#{noun[6]}that kissed ",
+			"#{noun[7]}that married ",
+			"#{noun[8]}that woke ",
+			"#{noun[9]}that kept ",
+			"#{noun[10]}that belonged to "
 			]
 	end
 
